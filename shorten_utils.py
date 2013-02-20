c2daa1088e053f506ffd38edfe62d35e4b4d7d7d
Phillips Opaola <fyamnky@gmail.com>
2013-02-20 14:04:53 -0500

    Update shorten_utils.py

--- shorten_utils.py
+++ shorten_utils.py
@@ -1,16 +1,11 @@
 import random
 
-# generate a random bit order
-# you'll need to save this mapping permanently, perhaps just hardcode it
-# map how ever many bits you need to represent your integer space
+#http://stackoverflow.com/a/1052896/2026118
 mapping = range(30)
 mapping.reverse()
-#random.shuffle(mapping)
 
-# alphabet for changing from base 10
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-# shuffle the bits
 def encode(n):
     result = 0
     for i, b in enumerate(mapping):
@@ -20,7 +15,6 @@ def encode(n):
             result |= b2
     return result
 
-# unshuffle the bits
 def decode(n):
     result = 0
     for i, b in enumerate(mapping):
@@ -30,14 +24,12 @@ def decode(n):
             result |= b1
     return result
 
-# change the base
 def enbase(x):
     n = len(chars)
     if x < n:
         return chars[x]
     return enbase(x/n) + chars[x%n]
 
-# go back to base 10
 def debase(x):
     n = len(chars)
     result = 0
